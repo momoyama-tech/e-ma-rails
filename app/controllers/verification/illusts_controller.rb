387ef29ac7a6405bb5d95ce8f55d9f699c988d7e
Hideyasu <kutuzov1228@gmail.com>
2025-04-27 03:14:46 +0900

    feat: update verification rest

--- app/controllers/verification/illusts_controller.rb
+++ app/controllers/verification/illusts_controller.rb
@@ -7,9 +7,9 @@ module Verification
           title: "新しいイラストが投稿されました！",
           description: "新しいイラストが投稿されました！",
           urls: {
-            wish: "https://static.wikia.nocookie.net/pkm/images/7/7a/0385.png/revision/latest?cb=20200226023541&path-prefix=ja",
-            name: "https://static.wikia.nocookie.net/pkm/images/5/50/0025.png/revision/latest?cb=20200226022539&path-prefix=ja",
-            illustration: "https://appmedia.jp/wp-content/uploads/2019/11/95b3429781493584eb1a4ed0d7360c8e.jpg"
+            wish: "https://e-ma-rails-staging-986464278422.asia-northeast1.run.app/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MTAyLCJwdXIiOiJibG9iX2lkIn19--1874ee30d41f23cad5d2d1541e0dbbe98cb82c1f/wish.png",
+            name: "https://e-ma-rails-staging-986464278422.asia-northeast1.run.app/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MTAxLCJwdXIiOiJibG9iX2lkIn19--6c48cebdb350a6203792932633a1f43ef4e9ed33/name.png",
+            illustration: "https://e-ma-rails-staging-986464278422.asia-northeast1.run.app/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MTAwLCJwdXIiOiJibG9iX2lkIn19--b7e0505bca12100612a31042b464319b4a72e828/illustration.png"
           }
         }
       })
