b02ebc724359baf4ad126e409c7f9b26f5ea9e6c
Hideyasu <kutuzov1228@gmail.com>
2025-05-04 13:33:52 +0900

    bug: old emas rest same url

--- app/controllers/old_emas_controller.rb
+++ app/controllers/old_emas_controller.rb
@@ -10,13 +10,13 @@ class OldEmasController < ApplicationController
             title: "古いイラストを受信しました",
             description: "古いイラストを受信しました",
             urls: {
-              wish: "https://e-ma-rails-staging-986464278422.asia-northeast1.run.app/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MTAyLCJwdXIiOiJibG9iX2lkIn19--1874ee30d41f23cad5d2d1541e0dbbe98cb82c1f/wish.png",
-              name: "https://e-ma-rails-staging-986464278422.asia-northeast1.run.app/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MTAxLCJwdXIiOiJibG9iX2lkIn19--6c48cebdb350a6203792932633a1f43ef4e9ed33/name.png",
-              illustration: "https://e-ma-rails-staging-986464278422.asia-northeast1.run.app/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsiZGF0YSI6MTAwLCJwdXIiOiJibG9iX2lkIn19--b7e0505bca12100612a31042b464319b4a72e828/illustration.png"
+              wish: ema.wish_image&.image&.attached? ? url_for(ema.wish_image.image) : nil,
+              name: ema.name_image&.image&.attached? ? url_for(ema.name_image.image) : nil,
+              illustration: ema.illustration&.image&.attached? ? url_for(ema.illustration.image) : nil
             }
           }
         })
-        sleep 1
+        sleep 10
       end
     end
 
